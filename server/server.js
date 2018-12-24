441301ac064d329ea5dd91b7e16e0aa9805b7d5d
jdoleo <deoleo14@hotmail.com>
2018-12-24 06:49:03 -0400

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,50 +1,24 @@
 require('./config/config');
 
+const mongoose = require('mongoose');
+
 const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-});
-
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre == undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es requerido'
-
-        }); // Bad Request;
-    } else {
-
-    }
+app.use(require('./routes/usuarios'));
 
 
-    res.json({
-        persona: body
-    });
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario');
+mongoose.connect(process.env.URLDB, (err, resp) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 
