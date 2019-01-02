b411401655fe33e6b297acf4b5e2b14b8c4170fd
jdoleo <deoleo14@hotmail.com>
2019-01-02 07:32:58 -0400

    Login normal seccion 10

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(require('./routes/usuarios'));
-
+// Configuracion global de rutas
+app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, (err, resp) => {
     if (err) throw err;
